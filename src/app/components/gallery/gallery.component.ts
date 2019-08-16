7d31e69d0bb047c578674e15c21fc0c5ffc4a6ce
Lucas Kimura <lucas.kimura@opus-software.com.br>
2019-08-16 13:31:06 -0300

    comecando galeria

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-
+  isVisible: boolean = true;
   constructor() { }
 
   ngOnInit() {
